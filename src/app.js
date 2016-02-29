a777e19620e23add7cfa530f089a9f8d31b862a5
Tim van Lent <lentvantim@hotmail.com>
2016-02-29 16:12:07 +0100

    some stylin

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+
 var Sequelize = require('sequelize');
 var express = require('express');
 var bodyParser = require('body-parser');
@@ -12,7 +13,11 @@ var sequelize = new Sequelize('blogapplication', 'timvanlent', null, {
 });
 
 var User = sequelize.define('user', {
-	username: Sequelize.STRING,
+	username: {
+		type: Sequelize.STRING,
+		unique: true,
+		allowNull: false
+	},
 	password: Sequelize.STRING,
 	email: Sequelize.STRING
 });
@@ -20,19 +25,37 @@ var User = sequelize.define('user', {
 var Blog = sequelize.define('blog', {
 	title: Sequelize.STRING,
 	body: Sequelize.TEXT,
+	userId: {
+		type: Sequelize.STRING,
+		references: {
+			model: User,
+			key: "username"
+		}
+	}
 });
 
 var Comment = sequelize.define('comment', {
 	comment: Sequelize.TEXT,
-}, {
-	timestamps: true
+	userId: {
+		type: Sequelize.STRING,
+		references: {
+			model: User,
+			key: "username"
+		}		
+	}
 });
 
 Blog.hasMany(Comment);
-Comment.belongsTo(User);
-User.hasMany(Blog);
+User.hasMany(Comment, {
+	targetKey: 'username'
+});
+User.hasMany(Blog, {
+	targetKey: 'username'
+});
 
 var app = express();
+
+app.use(express.static('./src/views'));
 
 app.use(session({
 	secret: 'fakka',
@@ -43,40 +66,11 @@ app.use(session({
 app.set('views', './src/views');
 app.set('view engine', 'jade');
 
-
-// app.get('/blog/:blogId', function(req, res) {
-// 	if (req.session.user != undefined) {
-// 		var blogID = req.params.blogId;
-// 		console.log('blogID:' + blogID)
-// 		Blog.findById(blogID)
-// 			.then(Comment.findAll({
-// 					where: {
-// 						blogId: blogID
-// 					}
-// 				}).then(function(comments) {
-// 					console.log(comments)
-// 					var comments = comments.map(function(row) {
-// 						return {
-// 							id: row.dataValues.id,
-// 							comment: row.dataValues.comment,
-// 						}
-// 					})
-// 				}).then(function() {
-// 					res.redirect('/', {
-// 						blogID: blogID,
-// 						// comments: comments
-// 					})
-// 				}));
-// 		} else {
-// 			res.redirect('/')
-// 		}
-// });
-
 app.get('/', function(req, res) {
 	Promise.all([ //
 		Blog.findAll(),
-		Comment.findAll(),
-		User.findAll()
+		User.findAll(),
+		Comment.findAll()
 	]).then(function(entities) {
 
 		var blogs = entities[0].map(function(row) {
@@ -89,7 +83,7 @@ app.get('/', function(req, res) {
 			};
 		});
 
-		var users = entities[2].map(function(row) {
+		var users = entities[1].map(function(row) {
 			return {
 				id: row.dataValues.id,
 				username: row.dataValues.username,
@@ -98,12 +92,20 @@ app.get('/', function(req, res) {
 			};
 		});
 
+		var comments = entities[2].map(function(row) {
+			return {
+				id: row.dataValues.id,
+				comment: row.dataValues.comment,
+				userId: row.dataValues.userId,
+				blogId: row.dataValues.blogId
+			};
+		});
+
 		res.render('index', {
 			blogs: blogs,
 			users: users,
-			// comments : comments,
+			comments : comments,
 			message: req.query.message,
-
 			user: req.session.user
 		});
 	});
@@ -112,11 +114,10 @@ app.get('/', function(req, res) {
 app.get('/blog/:blogId', function(req, res) {
 	Promise.all([ //
 		Blog.findAll(),
-		Comment.findAll(),
 		User.findAll()
 	]).then(function(entities) {
 
-		var blogs = entities[0].map(function(row) {
+		var data = entities[0].map(function(row) {
 
 			return {
 				id: row.dataValues.id,
@@ -126,33 +127,9 @@ app.get('/blog/:blogId', function(req, res) {
 			};
 
 		});
-		console.log(blogs);
-		blogID = req.params.blogId;
-		console.log('blogID:' + blogID)
+		blogs = data.reverse();
 
-		var comments = []
-		Blog.findById(blogID).then(Comment.findAll({
-			where: {
-				blogId: blogID
-			}
-		}).then(function(commentaar) {
-			var data = entities[1].map(function(row) {
-				return {
-					id: row.dataValues.id,
-					comment: row.dataValues.comment
-				}
-
-			})
-				comments = data.reverse();
-				console.log('comments innerfunction')
-				console.log(comments);
-			if (comments.length === 0) {
-				comments = ['no comments']
-				console.log(comments)
-			}
-
-		}));
-		var users = entities[2].map(function(row) {
+		users = entities[1].map(function(row) {
 			return {
 				id: row.dataValues.id,
 				username: row.dataValues.username,
@@ -160,19 +137,48 @@ app.get('/blog/:blogId', function(req, res) {
 				password: row.dataValues.password
 			};
 		});
-		console.log('comments:');
-		console.log(comments);
-		console.log('rendering:');
-
-		res.render('blog', {
-			blogs: blogs,
-			users: users,
-			comments: comments,
-			message: req.query.message,
-			user: req.session.user,
-			blogId: blogID
-		});
 	});
+
+	blogID = req.params.blogId;
+	Blog.findById(blogID).then(Comment.findAll({
+		where: {
+			blogId: blogID
+		}
+	}).then(function(commentaar) {
+		comments = commentaar.map(function(row) {
+			return {
+				id: row.dataValues.id,
+				comment: row.dataValues.comment,
+				userId: row.dataValues.userId
+			}
+
+		})
+		if (comments.length > 0) {
+			res.render('blog', {
+				blogs: blogs,
+				users: users,
+				comments: comments,
+				message: req.query.message,
+				user: req.session.user,
+				blogId: blogID,
+				userId: req.session.user.id 
+
+			});
+		} else {
+			comments = ['no comments']
+			res.render('blog', {
+				blogs: blogs,
+				users: users,
+				comments: comments,
+				message: req.query.message,
+				user: req.session.user,
+				blogId: blogID,
+				userId: req.session.user.id
+			});
+		}
+
+	}))
+
 });
 
 app.get('/logout', function(req, res) {
@@ -185,23 +191,26 @@ app.get('/logout', function(req, res) {
 });
 
 app.post('/login', bodyParser.urlencoded({
-		extended: true
-	}),
-	function(req, res) {
-		console.log("finding");
-		User.findOne({
-			where: {
-				username: req.body.username,
-				password: req.body.password
-			}
-		}).then(function(user) {
-			if (req.body.password === user.password) {
-				req.session.user = user;
-				res.redirect('/?message=' + encodeURIComponent("logged in as " + req.body.username))
-				console.log(user.dataValues);
-			}
-		});
+	extended: true
+}),
+function(req, res) {
+	User.findOne({
+		where: {
+			username: req.body.username
+		}
+	}).then(function(user) {
+		console.log('infunction')
+		if (user !== null && req.body.password === user.password) {
+			req.session.user = user;
+			res.redirect('/?message=' + encodeURIComponent("logged in as " + req.body.username))
+		} else {
+			res.redirect('/?message=' + encodeURIComponent("Invalid email or password."));
+		}
+	},
+	function (error) {
+		res.redirect('/?message=' + encodeURIComponent("Invalid email or password."));
 	});
+});
 
 app.post('/register', bodyParser.urlencoded({
 	extended: true
@@ -223,14 +232,12 @@ app.post('/register', bodyParser.urlencoded({
 app.post('/blogs', bodyParser.urlencoded({
 	extended: true
 }), function(req, res) {
-
 	Blog.create({
 		title: req.body.title,
 		body: req.body.body,
-		userId: req.session.user.id
+		userId: req.session.user.username
 	}).then(function(rows) {
-		console.log(rows.dataValues)
-		res.redirect('/');
+		res.redirect('/blog/' + rows.dataValues.id + '#' + rows.dataValues.id);
 	});
 });
 
@@ -241,10 +248,9 @@ app.post('/comment', bodyParser.urlencoded({
 }), function(req, res) {
 	Comment.create({
 		comment: req.body.comment,
-		userId: req.session.user.id,
+		userId: req.session.user.username,
 		blogId: blogID
 	}).then(function(rows) {
-		console.log(rows.dataValues)
 		res.redirect('/blog/' + blogID);
 	});
 });
